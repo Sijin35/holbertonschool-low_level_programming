fa83c568fe65b8df518212f0677ae541edc62a6a
Sijin35 <sijinsingh222@gmail.com>
2026-03-23 09:40:28 +0000

    Added include statement for stddef

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -9,6 +11,7 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
     char *str;
