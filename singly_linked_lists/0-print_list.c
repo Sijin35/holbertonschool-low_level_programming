269e6f7ce103d6580be67b1fc69928733ef56cd8
Sijin35 <sijinsingh222@gmail.com>
2026-03-23 09:42:45 +0000

    Happy Betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -17,17 +17,17 @@ size_t print_list(const list_t *h)
 
 	while (current != NULL)
 	{
-		if(current -> str == NULL)
+		if (current->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%u] %s\n", current -> len, current -> str);
+			printf("[%u] %s\n", current->len, current->str);
 		}
 
 		c++;
-		current = current -> next;
+		current = current->next;
 	}
 
 	return (c);
