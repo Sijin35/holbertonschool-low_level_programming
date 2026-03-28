4e08ead70778ff21e354ceada7399d9504c7236c
Sijin35 <sijinsingh222@gmail.com>
2026-03-28 10:21:26 +0000

    Started counter from 1 instead of 0

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 	dlistint_t *current = *h;
-	unsigned int c = 0;
+	unsigned int c = 1;
 
 	if (new == NULL)
 	{return (NULL); }
