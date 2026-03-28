e7e6f235e13d9cbeeed47a0945bb2c1e77db3081
Sijin35 <sijinsingh222@gmail.com>
2026-03-28 10:10:55 +0000

    Function taht inserts a new node at a given posiiton

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Inserts a new node at a given position
+ *
+ * @h: Start of node
+ *
+ * @idx: Index where new node should be added, starts at 0
+ *
+ * @n: Value
+ *
+ * Return: Address of new node or NULL if failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *current = *head;
+	unsigned int c = 0;
+
+	if (new == NULL)
+	{return (NULL); }
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = current;
+		new->prev = NULL;
+		if (current != NULL)
+		{
+			(*head)->prev = new;
+		}
+		*head = new;
+
+		return (new);
+	}
+	while (current != NULL)
+	{
+		if (c == idx)
+		{
+			new->next = current;
+			new->prev = current->prev;
+			current->prev->next = new;
+			current->prev = new;
+
+			return (new);
+		}
+
+		current = current->next;
+		c++;
+	}
+	return (NULL);
+}
