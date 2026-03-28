04ca253adae1b5fcdb73a3e01e29dc35ba9aefc7
Sijin35 <sijinsingh222@gmail.com>
2026-03-28 10:18:56 +0000

    Added new struct to test checker condition

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -21,6 +21,21 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
