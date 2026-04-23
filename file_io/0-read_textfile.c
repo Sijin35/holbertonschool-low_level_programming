e059fff2ca3a9ce021d710608d488f5d3366e5cc
Sijin <sijinsingh222@gmail.com>
2026-04-23 11:39:57 +1000

    Refactor read_textfile function for improved error handling

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,36 +13,37 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes, write_bytes;
 	int fd;
-	char *buffer;
+	char *buffer = malloc(letters);
+	ssize_t r, w;
 
-	if (filename == NULL)
-	{return (0); }
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{return (0); }
+	if (!filename)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		free(buffer);
 		return (0);
-	}
-	bytes = read(fd, buffer, letters);
-	if (bytes == -1)
+	if (!buffer)
 	{
 		close(fd);
-		free(buffer);
 		return (0);
 	}
-	write_bytes = write(1, buffer, bytes);
-	if (write_bytes != bytes)
+
+	r = read(fd, buffer, letters);
+	if (r == -1)
 	{
-		close(fd);
 		free(buffer);
+		close(fd);
 		return (0);
 	}
-	close(fd);
+
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1 || w != r)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
 	free(buffer);
-	return (bytes);
-}
+	close(fd);
+return (w);}
