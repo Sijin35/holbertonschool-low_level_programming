2bd8aa4a2b313a4da88b943ce6fe56e12574e905
Sijin <sijinsingh222@gmail.com>
2026-04-23 11:47:06 +1000

    Fix formatting and return statement in read_textfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -18,16 +18,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t r, w;
 
 	if (!filename)
+	{
 		return (0);
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
 		return (0);
+	}
 	if (!buffer)
 	{
 		close(fd);
 		return (0);
 	}
-
 	r = read(fd, buffer, letters);
 	if (r == -1)
 	{
@@ -35,7 +38,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	w = write(STDOUT_FILENO, buffer, r);
 	if (w == -1 || w != r)
 	{
@@ -43,7 +45,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	free(buffer);
 	close(fd);
-return (w);}
+	return (w);
+}
